c5dfea827b52076ce1553e003fb3e5b91faa9641
siascone <spencer.iascone@gmail.com>
2026-04-01 19:48:36 -0600

    Add db_create route to create Basketball table in db, pushing to test deployment

--- app.py
+++ app.py
@@ -14,3 +14,30 @@ def testing():
     
     conn.close() #remember to always close the db connection
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def db_create():
+    # establish db connection
+    conn = psycopg2.connect("postgresql://flask_hello_world_3308_db_zhm2_user:WGcP8mSCQvIZcVm71PiG7FbodJS6iuvp@dpg-d76ret15pdvs7384es0g-a/flask_hello_world_3308_db_zhm2")
+    
+    # open cursor to connection
+    cur = conn.cursor()
+    
+    # create Basketball table with psycopg2 SQL query string
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );            
+    ''')
+    
+    # commit the changes made by the query above to the db (i.e. save to the db)
+    conn.commit()
+    
+    # close db connection
+    conn.close()
+    
+    return "Basketball Table Successfully Created"

a67ff417d14b3660ae14a17930c45cce459ad6f3
siascone <spencer.iascone@gmail.com>
2026-04-01 20:05:42 -0600

    Add db_insert route and query to populate Basketball table with players, pushing to test deploy

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def testing():
     return "Database Connection Successful"
 
 @app.route('/db_create')
-def db_create():
+def creating():
     # establish db connection
     conn = psycopg2.connect("postgresql://flask_hello_world_3308_db_zhm2_user:WGcP8mSCQvIZcVm71PiG7FbodJS6iuvp@dpg-d76ret15pdvs7384es0g-a/flask_hello_world_3308_db_zhm2")
     
@@ -41,3 +41,29 @@ def db_create():
     conn.close()
     
     return "Basketball Table Successfully Created"
+
+@app.route('/db_insert')
+def inserting():
+    # establish db connection
+    conn = psycopg2.connect("postgresql://flask_hello_world_3308_db_zhm2_user:WGcP8mSCQvIZcVm71PiG7FbodJS6iuvp@dpg-d76ret15pdvs7384es0g-a/flask_hello_world_3308_db_zhm2")
+    
+    # open cursor to connection
+    cur = conn.cursor()
+    
+    # run sql query to populate basketball table with 4 players
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2); 
+    ''')
+    
+    # save insert to db
+    conn.commit()
+    
+    # close db connectino
+    conn.close()
+    
+    return "Basketball Table Successfully Populated"
